79c2a365c0222d0bb13b242e1ca132aa291a9da2
LuciaGangi <lucia.gangi@mi.unc.edu.ar>
2023-05-24 12:14:14 -0300

    todayDishes done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useEffect } from 'react';
 import Aos from 'aos';
 
-
 import './App.css';
 
 import NavBarSocial from './components/NavBarSocial';
 import NavBar from './components/NavBar';
 import Carousel from './components/Carousel';
+import Welcome from './components/Welcome';
+import TodayDishes from './components/TodayDishes';
 
 function App() {
   useEffect(() => {
@@ -20,6 +21,8 @@ function App() {
         <NavBar />
       </header>
       <Carousel/>
+      <Welcome />
+      <TodayDishes />
       <footer className="bg-gray-800 text-white py-4 px-8">Mi pie de página</footer>
     </div>
   );
